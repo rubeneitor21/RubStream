d266025f1b77fd1767c34e19476b40107d92b74b
Ruben Garcalia <rubeneitor2@hotmail.com>
2024-01-08 21:31:23 +0100

    Better practices

--- Client/Web/rub-stream/src/app/show/page.tsx
+++ Client/Web/rub-stream/src/app/show/page.tsx
@@ -1,15 +1,24 @@
 "use client"
-export default function Show() {
 
-    const videoInfo = JSON.parse(localStorage.getItem("video") || "{}")
-    const videoTime = JSON.parse(localStorage.getItem("current") || "{}")
-    let time = 0
-    if (videoInfo?.name === videoTime?.name) {
-        time = videoTime.time
-    }
+import { useEffect, useState } from "react"
+
+
+export default function Show() {
+    const [videoInfo, setVideoInfo] = useState({} as any)
+    const [videoTime, setVideoTime] = useState({} as any)
+
+    useEffect(() => {
+        const info = JSON.parse(localStorage.getItem("video") || "{}")
+        const time = JSON.parse(localStorage.getItem("current") || "{}")
+        setVideoInfo(info)
+        if (info?.name === time?.name) {
+            setVideoTime(time)
+        }
+    }, [])
+
     return (
         <main className="bg-background">
-            <video controls className="w-screen h-screen bg-backgorund" src={"http://localhost:5000" + videoInfo.href.replace("/show?", "")} onTimeUpdate={() => {
+            <video controls className="w-screen h-screen bg-backgorund" src={"http://localhost:5000" + videoInfo?.href?.replace("/show?", "")} onTimeUpdate={() => {
                 const video = document.querySelector("video") as HTMLVideoElement
                 localStorage.setItem("current", JSON.stringify({
                     "name": videoInfo.name,
@@ -17,7 +26,7 @@ export default function Show() {
                 }))
             }} onPlay={() => {
                 const video = document.querySelector("video") as HTMLVideoElement
-                video.currentTime = time
+                video.currentTime = videoTime.time
             }}></video>
         </main>
     )
